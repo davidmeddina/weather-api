1850fafd383279b6a38255d670eb676dd8970dfa
David <davidmeddina@gmail.com>
2020-06-22 18:02:14 -0500

    add location controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+l = Location.create(name: 'Medellín')
+l.recordings.create(temp: 32, status: 'sunny')
+l.recordings.create(temp: 30, status: 'sunny')
+l.recordings.create(temp: 34, status: 'sunny')
+l.recordings.create(temp: 28, status: 'cloudy')
+l.recordings.create(temp: 25, status: 'rainy')
