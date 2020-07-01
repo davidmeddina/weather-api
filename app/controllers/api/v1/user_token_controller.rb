39a79eb136206db295e30e27917c72d4c7c17d34
David <davidmeddina@gmail.com>
2020-06-30 22:57:05 -0500

    add authentication with knock

---
+++ app/controllers/api/v1/user_token_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::UserTokenController < Knock::AuthTokenController
+  skip_before_action :verify_authenticity_token, raise: false
+
+  def entity_name
+    'User'
+  end
+
+end
--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,13 @@ Bundler.require(*Rails.groups)
 module Weather
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.0 and config.autoloader = :classic
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # config/initializers/knock.rb 
+    config.token_secret_signature_key = -> { Rails.application.credentials.secret_key_base }
   end
 end
